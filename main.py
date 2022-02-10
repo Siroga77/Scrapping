081abf39cfc89b4b185bdc4aac56a7e4ebf4c5c7
crypto <ranan11@ukr.net>
2022-02-10 21:43:48 +0200

    2

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from requests import get
 hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
 public_ip = get('http://api.ipify.org').text
+print(".......................")
 print(f'Host: {hostname}')
 print(f'Local iP: {local_ip}')
 print(f'Public iP: {public_ip}')
-print(".......................")
+ent = input("Press \"e: \"")
+if ent == "e":
+    break
