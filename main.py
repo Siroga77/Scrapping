59536e2dcf452444968b6a1776b28d2619e02e26
crypto <ranan11@ukr.net>
2022-02-11 12:46:20 +0200

    Build trees

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import math
+import random
+
+
 class FieldElement:
 
     def __init__(self, num, prime):
@@ -18,3 +22,17 @@ class FieldElement:
                 return False
             return self.num == other.num and self,prime == other.prime
             pass
+
+def Merkel_tree():
+    total = int(input("Enter num of nodes: "))
+    max_depth = math.ceil(math.log(total, 2))
+    merkle_tree = []
+    for depth in range(max_depth + 1):
+        num_items = math.ceil(total / 2**(max_depth - depth))
+        hash = random.getrandbits(128)
+        level_hashes = [hash] * num_items
+        merkle_tree.append(level_hashes)
+    for level in merkle_tree:
+        print(level)
+
+Merkel_tree()
